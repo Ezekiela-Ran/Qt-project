0a420707f683d87446462faf29eef722c41d9925
Sambatra <randriamanalinatahinasambatra@gmail.com>
2026-02-28 15:22:16 +0300

    r

--- readme.md
+++
@@ -1,6 +0,0 @@
-STEP TO LAUNCH THE PROGRAM:
-
-    pip install -r requirements.txt
-
-    Run the program with python main.py
-
--- views/layouts/list_layout.py
+++
@@ -1,7 +0,0 @@
-from PySide6 import QtWidgets
-class ListLayout(QtWidgets.QWidget):
-    """
-    Widget principal qui contient MenuLayout et éventuellement d'autres widgets.
-    """
-    def __init__(self, parent=None):
-        super().__init__(parent)
